7549719ac73e4f90672df374affd4f57ca0a8d74
Anna Oleksiak <anna.ol3ksiak@gmail.com>
2018-05-17 18:01:03 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,35 @@
+def turn (board)
+  puts "Please enter 1-9:"
+  user_input = gets.strip
+  
+end
+
+def display_board (board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index (user_input)
+  user_input.to_i - 1
+end
+
+def valid_move? (board, index)
+  if index.between?(0, 8) && !position_taken?(board, index)
+    true
+  else
+    false
+  end
+end
+
+def position_taken? (board, index)
+  board[index] == 'X' || board[index] == 'O'
+end
+
+def move (board, index, token = 'X')
+  if valid_move?(board, index)
+    board[index] = token
+  end
+end
